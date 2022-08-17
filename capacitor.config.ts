72143007f6f77d6acd9bd42e501b569517b64b41
Jan Brunner <jan.brunner@lbs-logics.com>
2022-08-17 14:25:45 +0200

    Initial ommit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kaunstdadenga.toolbarbug',
+  appName: 'ToolbarBug',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
